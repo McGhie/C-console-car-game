a8101310531bbd4a367f35bd5dd3b86cd7d9ec81
Argyll <argyllmcghie@gmail.com>
2017-04-07 11:38:16 +1000

    Ready To Compile
    Everything is here

---
+++ carboard.c
@@ -0,0 +1,20 @@
+#include "carboard.h"
+
+int main()
+{
+    /**
+     * Here's the main function. You can write the "main menu" loop/code
+     * here or you can make separate functions - up to you.
+     */
+
+    /* TODO */
+
+    printf("Good bye! \n\n");
+
+    return EXIT_SUCCESS;
+}
+
+void showStudentInformation()
+{
+    /* TODO */
+}
---
+++ game.c
@@ -0,0 +1,23 @@
+#include "game.h"
+
+void playGame()
+{
+    Cell board[BOARD_HEIGHT][BOARD_WIDTH];
+
+    /**
+     * This is some sample code using the functions provided.
+     *
+     * Note that NULL is passed as the "Player * player" parameter as there is
+     * no player cell present on the board in this example.
+     */
+    initialiseBoard(board);
+    displayBoard(board, NULL);
+
+    loadBoard(board, BOARD_1);
+    displayBoard(board, NULL);
+
+    loadBoard(board, BOARD_2);
+    displayBoard(board, NULL);
+
+    /* TODO */
+}
